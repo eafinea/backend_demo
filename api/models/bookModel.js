2e8d347a25eb0dab70b1636f2f9825ca112cd5e6
eafinea <kouconca@gmail.com>
2024-08-14 08:24:30 +0700

    changes_model

--- api/models/bookModel.js
+++ api/models/bookModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const bookSchema = mongoose.Schema(
    {
-      title: { type: String, required: true },
-      author: { type: String, required: true },
+      title: { type: String, required: true, unique: true },
+      author: { type: String, required: true},
       genre: { type: String, required: true },
       description: { type: String, required: true },
       coverImageUrl: { type: String, default: 'https://online.visual-paradigm.com/repository/images/4783989f-5816-4cca-970b-e5223e3d2f19/book-covers-design/illustrated-fantasy-book-cover.png' },
